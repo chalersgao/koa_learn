fb0a3fa02291d2d527d26af8551289f87ae162a7
Gao Zhuohang <2298770577@qq.com>
2024-06-12 00:06:47 +0800

    错误处理

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -6,6 +6,28 @@ class UserControler{
         //获取数据
         //  console.log(ctx.request.body)
         const {user_name,password} =ctx.request.body
+
+         //合法性
+         if(!user_name ||!password){
+            console.error("用户名或密码为空",ctx.request.body)
+            ctx.status = 400
+            ctx.body = {
+              code:"10001",
+              message:"用户名或密码为空",
+              result:" ",
+            }
+            return
+         }
+         //合理性
+         if( userervice.getUserInfo({ user_name })) {
+            ctx.status = 409
+            ctx.body={
+                code:'10002',
+                message:'用户已经存在',
+                result:' ',
+            }
+            return
+         } 
          //处理数据
          const res = await userervice.createUser(user_name,password)
          //  console.log(res)
--- src/service/user.service.ts
+++ src/service/user.service.ts
@@ -1,16 +1,25 @@
+import { where } from "sequelize";
 const User1 = require("../model/user.model")
 class UserService {
   async createUser(user_name:any,   password:any) {
     //插入数据
-    //  User.createUser({
-    //表的字段
-    /*         user_name: user_name,
-        password:password, */
-    // })
     //await表达式:promise对象的值
     const res = await User1.create({ user_name, password });
     // console.log(res);
     return res.dataValues
+  }
+   async getUserInfo({id ,user_name,password,is_admin}:{id:string,user_name:string,password:string,is_admin:boolean}){
+    const whereOpt = {}
+    id && Object.assign(whereOpt,{id})
+    user_name && Object.assign(whereOpt,{user_name})
+    password && Object.assign(whereOpt,{password})
+    is_admin && Object.assign(whereOpt,{is_admin})
+
+    const res = await User1.findOne({
+      attributes:['id','user_name','password','is_admin'],
+      where:whereOpt
+    })
+    return res ? res.dataValues : null
   }
 }
 module.exports = UserService;
